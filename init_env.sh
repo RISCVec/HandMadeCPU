c96442671e09fc2fa4dd79892df8d521e561094a
RISCVec <165450259+RISCVec@users.noreply.github.com>
2024-04-04 13:15:31 +0800

    Add files via upload

---
+++ init_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt -y install build-essential gcc cmake git default-jdk unzip wget
+wget https://downloads.lightbend.com/scala/2.12.19/scala-2.12.19.zip
+unzip scala-2.12.19.zip
+sudo mv ./scala-2.12.19 /usr/local
+wget https://github.com/sbt/sbt/releases/download/v1.9.7/sbt-1.9.7.zip
+unzip sbt-1.9.7.zip
+sudo mv ./sbt /usr/local
+echo "PATH=\$PATH:/usr/local/scala-2.12.19/bin:/usr/local/sbt/bin" >> ~/.bashrc
+source .bashrc
