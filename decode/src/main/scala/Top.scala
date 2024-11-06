c7d4a939edbd1c81a850e383a6c7ec51c48fa46f
RISCVec <165450259+RISCVec@users.noreply.github.com>
2024-11-06 10:05:33 +0800

    Add files via upload

---
+++ decode/src/main/scala/Top.scala
@@ -0,0 +1,16 @@
+package decode
+
+import chisel3._
+import chisel3.util._
+import common.Consts._
+
+class Top extends Module {
+    val io = IO(new Bundle{
+        val exit = Output(Bool())
+    })
+
+    val core = Module(new Core())
+    val memory = Module(new Memory())
+    core.io.imem <> memory.io.imem
+    io.exit := core.io.exit
+}
